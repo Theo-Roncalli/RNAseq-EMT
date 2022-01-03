b0593dde193ae6d192fc7ef2f4a2963dae9faadd
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-03 11:46:52 +0100

    :triangular_flag_on_post: Modification of message for number of reads

--- import.sh
+++ import.sh
@@ -16,13 +16,13 @@ annotation_url=ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_2
 mkdir -p ${reads}
 echo "Downloading reads..."
 wget ${reads_url} -P ${reads}
-echo "Unarchive reads..."
+echo "Unarchiving reads..."
 tar -zxf ${reads}/TPrnaseq.tar.gz -C ${reads}
 
-echo "--------Number of sequences per file--------"
+echo "--------Number of reads per file--------"
 for file in ${reads}/*.fastq
 do
-	grep ^+$ ${file} | echo "${file}  $(wc -l)";
+	grep ^+$ ${file} | echo "${file}: $(wc -l) reads";
 done
 
 # Step 2: download reference genome
